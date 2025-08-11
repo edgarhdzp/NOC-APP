f6a3c317fe558a6ba049fe2d26bfa16970d5814d
Daniel Hernandez <edgardaniel1023@gmail.com>
2025-08-11 14:42:26 -0600

    feat: add mongo-db with docker

--- src/config/plugins/envs.plugins.ts
+++ src/config/plugins/envs.plugins.ts
@@ -7,4 +7,9 @@ export const envs = {
   EMAIL: env.get('EMAIL').required().asEmailString(),
   EMAIL_SECRET_KEY: env.get('EMAIL_SECRET_KEY').required().asString(),
   PROD: env.get('PROD').required().asBool(),
+
+  MONGO_URL: env.get('MONGO_URL').required().asString(),
+  MONGO_USER: env.get('MONGO_USER').required().asString(),
+  MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString()
 };
